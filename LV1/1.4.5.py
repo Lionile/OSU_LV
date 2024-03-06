ce5df63fde5067fcae003fbd1bae9a69de045a62
leon.maderic <leon.maderic@gmail.com>
2024-03-06 13:44:06 +0100

    count messages ending with exclamation

--- LV1/1.4.5.py
+++ LV1/1.4.5.py
@@ -2,6 +2,7 @@ file = open("SMSSpamCollection.txt")
 
 spamc = 0
 spam_wordc = 0
+spam_exclamation = 0
 hamc = 0
 ham_wordc = 0
 
@@ -9,6 +10,8 @@ for line in file:
     line = line.rstrip()
     if line.startswith("spam"):
         spamc += 1
+        if line.endswith("!"):
+            spam_exclamation += 1
         line = line[5:]
         for word in line.split():
             spam_wordc += 1
@@ -19,6 +22,7 @@ for line in file:
             ham_wordc += 1
 
 print("Spam messages: " + str(spamc) + ", average words: " + str(spam_wordc/spamc))
+print("Spam messages ending with exclamation: " + str(spam_exclamation))
 print("Ham messages: " + str(hamc) + ", average words: " + str(ham_wordc/hamc))
 
 file.close()

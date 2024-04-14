d5b97c14f072d861415b91b0005c95461f6df021
leonm <leon.maderic@gmail.com>
2024-04-14 20:55:49 +0200

    Add LV7

--- LV7/Zadatak_1.py
+++ LV7/Zadatak_1.py
@@ -38,13 +38,67 @@ def generate_data(n_samples, flagc):
         
     return X
 
-# generiranje podatkovnih primjera
-X = generate_data(500, 1)
 
-# prikazi primjere u obliku dijagrama rasprsenja
-plt.figure()
-plt.scatter(X[:,0],X[:,1])
-plt.xlabel('$x_1$')
-plt.ylabel('$x_2$')
-plt.title('podatkovni primjeri')
+# 1.1
+# creates a 2x3 grid (max 6 plots)
+def plot_data(X):
+    plt.figure()
+    for x in range(len(X)):
+        ax = plt.subplot(230 + x + 1)
+        plt.scatter(X[x][:,0],X[x][:,1])
+        plt.xlabel('$x_1$')
+        plt.ylabel('$x_2$')
+        plt.title('flagc = ' + str(x+1))
+
+    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+
+
+# generiranje podatkovnih primjera
+X1 = generate_data(500, 1)
+
+X2 = generate_data(500, 2)
+
+X3 = generate_data(500, 3)
+
+X4 = generate_data(500, 4)
+
+X5 = generate_data(500, 5)
+
+plot_data([X1, X2, X3, X4, X5])
+
+
+# 1.2
+# creates a 2x3 grid (max 6 plots)
+def fit_and_plot(X, k):
+    plt.figure()
+    km = KMeans(n_clusters = k, init ='random', n_init =5, random_state =0)
+    for i in range(len(X)):
+        km.fit(X[i])
+        labels = km.predict(X[i])
+        ax = plt.subplot(230 + i + 1)
+        ax.scatter(X[i][:,0],X[i][:,1], c=labels)
+        ax.set_xlabel('$x_1$')
+        ax.set_ylabel('$x_2$')
+        ax.set_title('flagx = ' + str(i+1))
+    
+    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    plt.suptitle('KMeans, k=' + str(k))
+
+
+# k = 3
+fit_and_plot([X1, X2, X3, X4, X5], 3)
+
+# k = 4
+fit_and_plot([X1, X2, X3, X4, X5], 4)
+
+# k = 5
+fit_and_plot([X1, X2, X3, X4, X5], 5)
+
+
+
+# 1.3
+# ?????
+
+
+
 plt.show()
--- LV7/Zadatak_2.py
+++ LV7/Zadatak_2.py
@@ -11,7 +11,6 @@ plt.figure()
 plt.title("Originalna slika")
 plt.imshow(img)
 plt.tight_layout()
-plt.show()
 
 # pretvori vrijednosti elemenata slike u raspon 0 do 1
 img = img.astype(np.float64) / 255
@@ -22,3 +21,118 @@ img_array = np.reshape(img, (w*h, d))
 
 # rezultatna slika
 img_array_aprox = img_array.copy()
+
+
+# 2.1
+pixels = {}
+
+for pixel in img_array_aprox:
+    pixels[tuple(pixel)] = 1
+
+print("Number of unique colors: ", len(pixels))
+
+
+# 2.2, 2.3, 2.4
+
+def predict(img, k):
+    km = KMeans(n_clusters=k, init='random', n_init=5, random_state=0)
+    km.fit(img)
+    labels = km.predict(img)
+    centers = km.cluster_centers_
+    
+    img_array_aprox_kmeans = img.copy()
+    for i in range(len(img)):
+        img_array_aprox_kmeans[i] = centers[labels[i]]
+    
+    j = km.inertia_
+
+    return [labels, centers, img_array_aprox_kmeans, j]
+
+def predict_and_plot(img, k, plot_scatter=False):
+    labels, centers, img_array_aprox_kmeans, j = predict(img, k)
+    
+    if plot_scatter:
+        plt.figure()
+        plt.scatter(img[::10,0], img[::10,1], img[::10,2], c=labels[::10])
+    
+    plt.figure()
+    plt.title("Image after KMeans (k=" + str(k) + ")")
+    plt.imshow(np.reshape(img_array_aprox_kmeans, (w,h,d)))
+    plt.tight_layout()
+    
+    return [labels, centers, img_array_aprox_kmeans, j]
+
+# k = 5
+predict_and_plot(img_array_aprox, 5, plot_scatter=True)
+
+
+# k = 8
+predict_and_plot(img_array_aprox, 8)
+
+
+# k = 15
+predict_and_plot(img_array_aprox, 15)
+
+
+# 2.5
+# test_4.jpg
+img = Image.imread("imgs\\test_3.jpg")
+img = img.astype(np.float64) / 255
+w,h,d = img.shape
+img_array = np.reshape(img, (w*h, d))
+img_array_aprox = img_array.copy()
+
+# k = 8
+predict_and_plot(img_array_aprox, 8)
+
+
+# test_6.jpg
+img = Image.imread("imgs\\test_6.jpg")
+img = img.astype(np.float64) / 255
+w,h,d = img.shape
+img_array = np.reshape(img, (w*h, d))
+img_array_aprox = img_array.copy()
+
+# k = 8
+predict_and_plot(img_array_aprox, 8)
+
+
+# 2.6
+img = Image.imread("imgs\\test_3.jpg")
+img = img.astype(np.float64) / 255
+w,h,d = img.shape
+img_array = np.reshape(img, (w*h, d))
+img_array_aprox = img_array.copy()
+
+labels, centers, img_array_aprox_kmeans, j1 = predict(img_array_aprox, 3)
+labels, centers, img_array_aprox_kmeans, j2 = predict(img_array_aprox, 4)
+labels, centers, img_array_aprox_kmeans, j3 = predict(img_array_aprox, 5)
+labels, centers, img_array_aprox_kmeans, j4 = predict(img_array_aprox, 6)
+labels, centers, img_array_aprox_kmeans, j5 = predict(img_array_aprox, 7)
+labels, centers, img_array_aprox_kmeans, j6 = predict(img_array_aprox, 8)
+labels, centers, img_array_aprox_kmeans, j7 = predict(img_array_aprox, 9)
+labels, centers, img_array_aprox_kmeans, j8 = predict(img_array_aprox, 10)
+labels, centers, img_array_aprox_kmeans, j9 = predict(img_array_aprox, 11)
+labels, centers, img_array_aprox_kmeans, j10 = predict(img_array_aprox, 12)
+
+j = [j1, j2, j3, j4, j5, j6, j7, j8, j9, j10]
+
+plt.figure()
+plt.scatter(range(3, 13), j)
+
+
+# 2.7
+labels, centers, img_array_aprox_kmeans, j3 = predict(img_array_aprox, 5)
+
+for i in range(len(img_array_aprox_kmeans)):
+    if(img_array_aprox_kmeans[i] == centers[1]).all():
+        img_array_aprox_kmeans[i] = [0, 0, 0]
+    else:
+        img_array_aprox_kmeans[i] = [1, 1, 1]
+        
+
+plt.figure()
+plt.title("One color group after KMeans")
+plt.imshow(np.reshape(img_array_aprox_kmeans, (w,h,d)))
+
+plt.show()
